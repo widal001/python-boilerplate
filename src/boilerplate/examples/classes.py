8e58ddf43133346ff778310f1582c44da6febba2
widal001 <williamdaly422@gmail.com>
2021-06-15 20:34:46 -0400

    Adds a property method to Person class and disables some warnings

--- src/boilerplate/examples/classes.py
+++ src/boilerplate/examples/classes.py
@@ -1,3 +1,17 @@
 class Person:
-    def __init__(self, name: str) -> None:
-        self.name = name
+    """Example implementation of a class"""
+
+    def __init__(
+        self,
+        first_name: str,
+        last_name: str,
+        age: int,
+    ) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    @property
+    def full_name(self):
+        """Returns the Person's full name"""
+        return f"{self.first_name} {self.last_name}"
--- tests/examples/test_classes.py
+++ tests/examples/test_classes.py
@@ -3,9 +3,28 @@ import pytest
 from boilerplate.examples import Person
 
 
-@pytest.mark.parametrize("name", ["Joe Biden", "Kamala Harris"])
-def test_person(name):
-    # execution
-    p = Person(name)
-    # validation
-    assert p.name == name
+class TestPerson:
+    """Tests for the Person class"""
+
+    @pytest.mark.parametrize(
+        "first_name, last_name, age",
+        [
+            ("Joe", "Biden", 78),
+            ("Kamala", "Harris", 56),
+        ],
+    )
+    def test_init(self, first_name, last_name, age):
+        """Tests init for Person, and validates that:
+        - All of the init parameters match class attributes
+        - The value of self.full_name is the concatenation of first_name
+          and last_name with a space between
+        """
+        # setup
+        full = f"{first_name} {last_name}"
+        # execution
+        person = Person(first_name, last_name, age)
+        # validation
+        assert person.full_name == full
+        assert person.age == age
+        assert person.first_name == first_name
+        assert person.last_name == last_name
